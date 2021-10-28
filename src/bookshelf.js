74339e8acb4e1df55008506418cee7c9f1ccd981
Ian Wang <light-0100@outlook.com>
2021-10-28 23:38:58 +0800

    - can display books by categories

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -22,24 +22,68 @@ class Shelf extends React.Component {
 
     render() {
         return (
-            <div className="shelf">
-                {this.state.books.map((b) => {
-                    return (<p key={b.id}>{b.title} | {b.shelf}</p>)
-                })}
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Reading</h2>
+                <div className="bookshelf-books">
+                    <Book books={this.state.reading}/>
+                </div>
 
+                <h2 className="bookshelf-title">Want To Read</h2>
+                <div className="bookshelf-books">
+                    <Book books={this.state.wantToRead}/>
+                </div>
+
+                <h2 className="bookshelf-title">Already Read</h2>
+                <div className="bookshelf-books">
+                    <Book books={this.state.read}/>
+                </div>
             </div>
         )
     }
 
     groupByShelf = () => {
-        let reading = this.state.books.filter((b)=> b.shelf === "currentlyReading")
-        let wantTo = this.state.books.filter((b)=> b.shelf === "wantToRead")
-        let read = this.state.books.filter((b)=> b.shelf === "read")
+        let reading = this.state.books.filter((b) => b.shelf === "currentlyReading")
+        let wantTo = this.state.books.filter((b) => b.shelf === "wantToRead")
+        let read = this.state.books.filter((b) => b.shelf === "read")
         this.setState({reading: reading})
         this.setState({wantToRead: wantTo})
         this.setState({read: read})
     }
 }
 
+class Book extends React.Component {
+    render() {
+        const books = this.props.books
+
+        return <ol className="books-grid">
+            {
+                books.map((b) => (
+                    <li key={b.id}>
+                        <div className="book">
+                            <div className="book-top">
+                                <div className="book-cover" style={{
+                                    width: 128,
+                                    height: 193,
+                                    backgroundImage: "url(" + b.imageLinks.thumbnail + ")"
+                                }}></div>
+                                <div className="book-shelf-changer">
+                                    <select>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="book-title">{b.title}</div>
+                            <div className="book-authors">{b.authors.toString()}</div>
+                        </div>
+                    </li>
+                ))
+            }
+        </ol>
+    }
+}
 
 export default Shelf
